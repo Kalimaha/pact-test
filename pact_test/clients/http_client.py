98cdfc4e0dce1f9471b045ebe314b3981af1e1b0
Kalimaha <guido.barbaglia@gmail.com>
2017-06-12 15:40:08 +1000

    Switch to Requests: HTTP for Humans

--- pact_test/clients/http_client.py
+++ pact_test/clients/http_client.py
@@ -1,59 +1,48 @@
-import json
+import requests
 from pact_test.models.response import PactResponse
-
-from pact_test.utils.http_utils import build_request_from_interaction
-try:                                                # pragma: no cover
-    from urllib.request import Request, urlopen     # pragma: no cover
-except:                                             # pragma: no cover
-    from urllib2 import Request, urlopen            # pragma: no cover
 
 
 TEXT = 'text'
+JSON = 'application/json'
 
 
 def execute_interaction_request(url, port, interaction):
-    request = _pact2python_request(url, port, interaction)
-    return _python2pact_response(urlopen(request))
+    url = _build_url(url, port, interaction)
 
-
-def _python2pact_response(response):
-    status = _parse_status(response)
-    headers = _parse_headers(response)
+    server_response = requests.request('GET', url=url)
+    headers = _parse_headers(server_response)
     content_type = _get_content_type(headers)
 
     return PactResponse(
+        status=server_response.status_code,
         headers=headers,
-        status=status,
-        body=_parse_body(response, content_type)
+        body=_parse_body(server_response, content_type)
     )
 
 
-def _parse_status(response):
-    try:
-        status = response.status
-    except AttributeError:
-        status = response.code
-    return status
+def _parse_body(server_response, content_type):
+    if JSON in content_type:
+        return server_response.json()
+    else:
+        return server_response.text()
 
 
-def _parse_headers(response):
-    try:
-        headers = response.getheaders()
-    except AttributeError:
-        headers = []
-        for h in response.headers.headers:
-            key_value = h.split(':')
-            key = key_value[0].strip()
-            val = key_value[1].strip()
-            headers.append((key, val))
+def _parse_headers(server_response):
+    headers = []
+    server_headers = server_response.headers
+    for key in server_headers:
+        headers.append((key, server_headers[key]))
     return headers
 
 
-def _parse_body(response, content_type):
-    body = response.read().decode('utf-8')
-    if content_type == TEXT:
-        return body
-    return json.loads(body)
+def _build_url(url, port, interaction):
+    path = interaction['request'].get('path', '')
+    query = interaction['request'].get('query', '')
+
+    test_port = str(port) if port else str(80)
+    test_url = 'http://' + url + ':' + test_port + path + query
+
+    return test_url
 
 
 def _get_content_type(headers):
@@ -62,19 +51,3 @@ def _get_content_type(headers):
     if types:
         content_type = types[0][1]
     return content_type
-
-
-def _pact2python_request(url, port, interaction):
-    pact_request = build_request_from_interaction(interaction)
-
-    request_port = str(port) if port else '80'
-    request_url = 'http://' + url + ':' + request_port
-
-    request = Request(request_url)
-    request.data = bytearray().extend(map(ord, json.dumps(pact_request.body)))
-    request.method = pact_request.method
-    for key, value in pact_request.headers:
-        request.add_header(key, value)
-    request.selector = pact_request.path + pact_request.query
-
-    return request
