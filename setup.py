8ef32a54481dc66a11a121f3009ba28a1a9d2390
Guido Barbaglia <guido.barbaglia@rea-group.com>
2017-08-31 16:30:14 +1000

    Update version to debug Pyramid

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='pact-test',
-    version='0.2.0',
+    version='0.2.2',
     author='Guido Barbaglia',
     author_email='guido.barbaglia@gmail.com',
     packages=find_packages(),
